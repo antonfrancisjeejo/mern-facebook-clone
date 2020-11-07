232cb56864a10ad12ad4958588b6c0ebe5e13fb0
unknown <anton13jeejo@gmail.com>
2020-11-07 23:00:21 +0530

    Initial commit

---
+++ client/src/Components/Feed.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./Feed.css";
+import MessageSender from "./MessageSender";
+import Post from "./Post";
+import StoryReel from "./StoryReel";
+import axios from "../axios";
+import Pusher from "pusher-js";
+
+const pusher = new Pusher("878b37735eddd229253c", {
+  cluster: "ap2",
+});
+
+const Feed = () => {
+  const [profilePic, setProfilePic] = useState("");
+  const [postsData, setPostsData] = useState([]);
+
+  const syncFeed = () => {
+    axios.get("/retrieve/posts").then((res) => {
+      console.log(res.data);
+      setPostsData(res.data);
+    });
+  };
+
+  useEffect(() => {
+    const channel = pusher.subscribe("posts");
+    channel.bind("inserted", function (data) {
+      syncFeed();
+    });
+  }, []);
+
+  useEffect(() => {
+    syncFeed();
+  }, []);
+
+  return (
+    <div className="feed">
+      <StoryReel />
+      <MessageSender />
+
+      {postsData.map((entry) => (
+        <Post
+          profilePic={entry.avatar}
+          message={entry.text}
+          timestamp={entry.timestamp}
+          imgName={entry.imgName}
+          username={entry.user}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Feed;
